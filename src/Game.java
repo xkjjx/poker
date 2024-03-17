11ded7a350396c2ba46796781e9444b0029c3a7b
Kevin Johnson <john2003.kjj@gmail.com>
2024-03-17 12:46:45 -0500

    added Game.java

---
+++ src/Game.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Game {
+    //private static ArrayList<String> actions = new ArrayList<{"Check", "Call", "Raise", "Fold"}>();
+    private Deck deck;
+    private ArrayList<Player> players;
+    private int numPlayers;
+    private ArrayList<String> history;
+}
